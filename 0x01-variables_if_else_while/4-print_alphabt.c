be82c88cbb8c39574e355059b7e85b6a12a2de54
TumeloMash97 <legadomash97@gmail.com>
2023-06-19 07:45:04 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the letters except q and e
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+        char alph[24] = "abcdfghijklmnoprstuvwxyz";
+        int num = 0;
+
+        while (num < 24)
+        {
+                putchar(alph[num]);
+                num++;
+        }
+        putchar('\n');
+        return (0);
+}
