15c9222cd2f38ede27cde686415121a748678022
TumeloMash97 <legadomash97@gmail.com>
2023-06-22 15:13:03 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,13 +8,13 @@
 
 void print_square(int size)
 {
-	int x, y;
+int x, y;
 
 if (size > 0)
 {
 for (y = 0; y < size; y++)
 {
-for (x = 0; y < size; x++)
+for (x = 0; x < size; x++)
 {
 _putchar(35);
 }
